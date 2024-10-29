6589d626028771c7377a579b87a6ff9daae70fb2
Mathias <mat.moen@gmail.com>
2024-10-29 09:33:20 +0100

    todo +

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
                 <Breadcrumbs />
               </div>
             </header>
-            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <div className="flex flex-1 flex-col gap-4 p-4 pt-0"> {/* // TODO: Make scrollable etc */}
               {children}
             </div>
           </main>
