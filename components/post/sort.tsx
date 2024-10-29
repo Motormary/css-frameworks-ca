24e6f25dcc19631533121ede773742798f2159fc
Mathias <mat.moen@gmail.com>
2024-10-29 14:22:44 +0100

    feat: delete sort value

--- components/post/sort.tsx
+++ components/post/sort.tsx
@@ -6,6 +6,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { Button } from "../ui/button"
+import { X } from "lucide-react"
 
 export default function SortPosts({
   value,
@@ -15,16 +17,27 @@ export default function SortPosts({
   setValue: (value: string | undefined) => void
 }) {
   return (
-    <Select value={value} onValueChange={setValue} name="sort">
-      <SelectTrigger>
-        <SelectValue placeholder="Sort by" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectItem value="newest">Newest</SelectItem>
-          <SelectItem value="oldest">Oldest</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <>
+      <Select value={value} onValueChange={setValue} name="sort">
+        <SelectTrigger>
+          <SelectValue placeholder="Sort by" />
+        </SelectTrigger>
+        {value ? (
+          <Button
+            onClick={(e) => {
+              e.preventDefault()
+              setValue("")
+            }}>
+            <X />
+          </Button>
+        ) : null}
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="newest">Newest</SelectItem>
+            <SelectItem value="oldest">Oldest</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </>
   )
 }
