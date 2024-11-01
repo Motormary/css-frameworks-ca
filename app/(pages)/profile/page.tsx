99358f67b32a06dc3f1b81441f2ea4d1a0424c36
Mathias <mat.moen@gmail.com>
2024-11-01 14:47:54 +0100

    css: profile list

--- app/(pages)/profile/page.tsx
+++ app/(pages)/profile/page.tsx
@@ -1,11 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import checkUser from "@/src/actions/auth/check-cookie"
 import { getAllProfiles } from "@/src/actions/profile/get-all-profiles"
-import Link from "next/link"
 import { redirect } from "next/navigation"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Newspaper, User, UserPlus } from "lucide-react"
-import avatar from "assets/icons/user.svg"
+import UserCard from "@/components/profile/user-card"
 
 export default async function ProfilesPage() {
   const auth = await checkUser()
@@ -13,41 +10,9 @@ export default async function ProfilesPage() {
 
   const profiles = await getAllProfiles()
   return (
-    <ol className="flex flex-col gap-4 p-4">
+    <ol className="grid grid-cols-1 md:grid-cols-[repeat(auto-fit,minmax(270px,1fr))] lg:grid-cols-2 gap-4 p-4">
       {profiles.map((profile, index) => {
-        return (
-          <li
-            key={profile.name}
-            // flex flex-col items-center justify-center
-            className="relative grid grid-rows-3 grid-cols-3 items-center justify-center space-x-4 space-y-2 m-auto border p-4 rounded-md min-w-[10rem] hover:bg-muted">
-            <Avatar className="row-span-3 col-span-1 size-24">
-              <AvatarImage
-                src={
-                  profile?.avatar && profile?.avatar !== ""
-                    ? profile.avatar
-                    : undefined
-                }
-                alt="Avatar"
-              />
-              <AvatarFallback>
-                <User className="size-20" />
-              </AvatarFallback>
-            </Avatar>
-            <Link
-              className="absolute inset-0"
-              href={`/profile/${profile.name}`}></Link>
-            <span className="col-span-2 text-lg">{profile.name}</span>
-            <div className="flex gap-2 col-span-2">
-              <span className="flex gap-2 border rounded-full py-1 px-2">
-                <UserPlus /> {profile._count.followers}
-              </span>
-              <span className="flex items-center gap-2 col-span-2">
-                <Newspaper />
-                {profile._count.posts}
-              </span>
-            </div>
-          </li>
-        )
+        return <UserCard key={profile.name} profile={profile} />
       })}
     </ol>
   )
---
+++ components/profile/user-card.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import { UserData } from "@/src/actions/auth/types"
+import { Profile } from "@/src/actions/profile/types"
+import Link from "next/link"
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
+import { LoaderIcon, User } from "lucide-react"
+import { Button } from "../ui/button"
+import { toast } from "sonner"
+import { useState } from "react"
+
+export default function UserCard({ profile }: { profile: Profile }) {
+  const [isLoading, setIsLoading] = useState(false)
+  return (
+    <li
+      key={profile.name}
+      className="relative flex max-xs:flex-col max-xs:items-center gap-4 m-auto border p-4 rounded-md min-w-[12rem] w-full hover:bg-muted/60">
+      <Link
+        className="absolute inset-0 z-20"
+        href={`/profile/${profile.name}`}></Link>
+
+      <span
+        title={profile.name}
+        className="relative text-lg overflow-hidden max-sm:text-center w-full truncate xs:hidden z-50">
+        {profile.name} asdas babbaab s
+      </span>
+      {/* Avatar box */}
+      <div className="flex flex-col gap-2 items-center">
+        <Avatar className="size-24 min-w-fit border border-border">
+          <AvatarImage
+            src={
+              profile?.avatar && profile?.avatar !== ""
+                ? profile.avatar
+                : undefined
+            }
+            alt="Avatar"
+          />
+          <AvatarFallback>
+            <User className="size-20" />
+          </AvatarFallback>
+        </Avatar>
+        {/* Info Box */}
+        <div className="flex gap-2 col-span-full">
+          {/* Followers */}
+          <span className="border rounded-full py-1 px-2 text-sm text-nowrap">
+            {profile._count.followers} Followers
+          </span>
+          {/* Posts */}
+          <span className="border rounded-full py-1 px-2 text-sm text-nowrap">
+            {profile._count.posts} Posts
+          </span>
+        </div>
+      </div>
+
+      {/* Name */}
+      <div className="w-full flex flex-col justify-between gap-4 max-sm:text-center overflow-hidden cursor-default">
+        <span
+          title={profile.name}
+          className="relative text-lg overflow-hidden max-sm:text-center w-full truncate max-xs:hidden z-50">
+          {profile.name}
+        </span>
+        <div className="relative flex z-50">
+          <Button
+            disabled={isLoading}
+            onClick={(e) => {
+              setIsLoading(true)
+              toast.success(`You're now following ${profile.name}`)
+              setTimeout(() => setIsLoading(false), 500)
+            }}
+            variant="outline"
+            className="h-8 w-full rounded-full">
+            {isLoading ? (
+              <LoaderIcon className="animate-spin duration-2000" />
+            ) : (
+              "Follow"
+            )}
+          </Button>
+        </div>
+      </div>
+    </li>
+  )
+}
